178df6988b6676069d23616248d70df44b2ec9e3
urThomas <thomas.norum.ur@gmail.com>
2015-03-13 14:07:31 +0100

    heihei

--- src/main.go
+++ src/main.go
@@ -10,8 +10,8 @@ import (
 
 func main(){
 	
-	
-	state1 := netmod.State{LocalIP() ,"11", "22", "heihei"}
+	localIP, _ := LocalIP()
+	state1 := netmod.State{LocalIP ,"11", "22", "heihei"}
 	Client_map := make(map[string]State)	
 
 	go netmod.Read_status(Client_map)

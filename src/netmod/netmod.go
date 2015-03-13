d675f77b1d9dece22bb66832cf5f03afeeef85a0
urThomas <thomas.norum.ur@gmail.com>
2015-03-13 13:14:22 +0100

    heihei

--- src/netmod/netmod.go
+++ src/netmod/netmod.go
@@ -9,7 +9,9 @@ import (
 
 func Send_status(state1 State) {
 	baddr, err_conv_ip := net.ResolveUDPAddr("udp", "129.241.187.255:20020")
-	Check_error(err_conv_ip)
+	if err_conv_ip != nil {
+			fmt.Println("error:", err_conv_ip)
+		}
 	status_sender, err_dialudp := net.DialUDP("udp", nil, baddr)
 	Check_error(err_dialudp)
 	for {
@@ -17,12 +19,12 @@ func Send_status(state1 State) {
 		b, err_Json := json.Marshal(state1)
         if err_Json != nil {
 	        fmt.Println("error with JSON")
-	        fmt.Println(err1)
+	        fmt.Println(err_Json)
         }
 		n, err1 := status_sender.Write(b)
         if err1 != nil {
                 fmt.Println("error writing data to server")
-                fmt.Println(err)
+                fmt.Println(err1)
                 return
         }
 	}
@@ -31,9 +33,13 @@ func Send_status(state1 State) {
 
 func Read_status(Client_map map[State]int) {
 	laddr, err_conv_ip_listen := net.ResolveUDPAddr("udp", ":20020")
-	Check_error(err_conv_ip_listen)
+	if err_conv_ip_listen != nil {
+			fmt.Println("error:", err_conv_ip_listen)
+		}
 	status_receiver, err_listen := net.ListenUDP("udp", laddr)
-	Check_error(err_listen)
+	if err_listen != nil {
+			fmt.Println("error:", err_listen)
+		}
 	for {
 		time.Sleep(1000 * time.Millisecond)
 		b := make([]byte, 1024)

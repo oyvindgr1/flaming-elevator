e164360b9c6f43bea965eacc04e96443ff156883
oyvindgr1 <oyvigre@stud.ntnu.no>
2015-03-23 14:48:37 +0100

    Update fsm.go

--- src/statemachine/fsm.go
+++ src/statemachine/fsm.go
@@ -1,10 +1,41 @@
 package statemachine
 
 import(
+	"driver"
 	
 	
 )
 
+const(
+	Upwards   = 1
+	Downwards = -1
+	Rest = 0
+)
 
-func eventHandler() {
+func ElevatorInit() {
+	if driver.ElevInit() == 0 {
+		return 0
+	} else {
+		if driver.ElevGetFloorSensorSignal() != -1 {
+		} else {
+			driver.ElevSetSpeed(Downwards*300)
+			floor := driver.ElevGetFloorSensorSignal()
+			for floor == -1 {
+				floor = driver.ElevGetFloorSensorSignal()
+			}
+			ElevatorBrake()
+		}
+		fmt.Printf("Initialized\n")
+		return 1
+	}
 }
+
+func EventHandler() {
+}
+
+func ElevatorBrake() {
+	driver.ElevSetSpeed(Upwards*300)
+	time.After(time.Millisecond*20)
+}
+	
+	

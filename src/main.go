502b7ffa0e6640b3086b2f68b273fa9af5b722a5
oyvindgr1 <oyvigre@stud.ntnu.no>
2015-04-22 10:06:38 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -15,7 +15,8 @@ func main() {
 	
 	statusmap_chan := make(chan map[string]elevtypes.Status, 1)
 	status_chan := make(chan elevtypes.Status, 1)
-
+	
+	orders_todo_elev_chan := make(chan [elevtypes.N_FLOORS][elevtypes.N_BUTTONS]int, 1)
 	orders_local_elev_chan := make(chan [elevtypes.N_FLOORS][elevtypes.N_BUTTONS]int, 1)
 	orders_external_elev_chan := make(chan [elevtypes.N_FLOORS][elevtypes.N_BUTTONS - 1]int, 1)
 
@@ -62,10 +63,11 @@ func main() {
 	
 	
 	go order.OrderListener(orders_local_elev_chan, orders_external_elev_chan)
-	go order.OrdersFromNetwork(orders_local_elev_chan, statusmap_chan,orders_external_elev_chan)
+	go order.OrdersFromNetwork(orders_local_elev_chan, statusmap_chan,orders_external_elev_chan, orders_todo_elev_chan)
+	go order.ErrorRecovery()
 	
 	go network.SendStatus(status_chan)
-	go network.ReadStatus(statusmap_chan, netIsAlive)
+	go network.ReadStatus(statusmap_chan, netIsAlive, orders_todo_elev_chan)
 
 
 	go statemachine.StateMachine(orders_local_elev_chan, orders_external_elev_chan, status_chan)
